5507adc1b25468f726f06d4133d9c403620fcb9d
Abdurrahman YAKAR <ryakar75@gmail.com>
2025-03-02 17:03:47 +0300

    feat: update some files

--- tokenization.py
+++ tokenization.py
@@ -1,5 +1,4 @@
-
-import nltk # natural language toolkit
+import nltk  # natural language toolkit
 
 nltk.data.path.append(r"C:\nlp_learning\btkAkademiNLP\nltk_data")  # Özel klasör yolu
 nltk.download("punkt", download_dir=r"C:\nlp_learning\btkAkademiNLP\nltk_data")
@@ -11,6 +10,5 @@ text = "Hello, World! How are you? Hello, hi ..."
 # # noktalama işaretleri ve boşluklar ayrı birer token olarak elde edilecektir.
 word_tokens = nltk.word_tokenize(text)
 print(word_tokens)
-
 
 # Cümle tokenizasyonu: sent_tokenize: metni cümlelere ayırır. Her bir cümle bir toke olur.
