258b43d12270ae99f4f07eb31ee96e38bc7fa42f
Abdurrahman YAKAR <ryakar75@gmail.com>
2025-03-03 05:25:37 +0300

    feat: update some files

---
+++ stemming_lemmatization.py
@@ -0,0 +1,26 @@
+import nltk
+#%% Download data
+nltk.data.path.append(r"C:\nlp_learning\btkAkademiNLP\nltk_data")  # Özel klasör yolu
+#wordnet: lemmatization işlemi için gerekli veritabanı
+nltk.download("wordnet", download_dir=r"C:\nlp_learning\btkAkademiNLP\nltk_data")
+
+
+#%% Stemming
+from nltk.stem import PorterStemmer # Stemming için fonksiyon
+
+# porter stemmer nesnesini oluştur
+stemmer = PorterStemmer()
+words = ["running", "runner", "ran", "runs", "better", "go", "went"]
+
+# Kelimelerin stem'lerini buluyoruz, bunu yaparken de porter stemmerin stem() fonksiyonunu kullanıyoruz
+stems = [stemmer.stem(w) for w in words]
+print(f"stems: {stems}")
+
+# %% Lemmatization
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+words = ["running", "runner", "ran", "runs", "better", "go", "went"]
+lemmas = [lemmatizer.lemmatize(w, pos="v") for w in words]
+print(f"lemmas: {lemmas}")
