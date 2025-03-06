c8e0926f51811232620a5daea355b027156931d8
Abdurrahman YAKAR <ryakar75@gmail.com>
2025-03-06 09:48:18 +0300

    feat: send some files

---
+++ bag_of_words_imdb_dataset.py
@@ -0,0 +1,38 @@
+# import libraries
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+import re
+from collections import Counter
+
+
+
+
+# %% veri setinin içeriye aktarılması
+
+
+
+
+
+# %% metin temizleme
+
+
+
+
+# %% Bag Of Words
+
+
+# vectorizer tanımla
+
+
+# metin --> sayısal hale getir
+
+
+
+# kelime kümesi göster
+
+
+
+# vektör temsili göster
+
+
+# kelime frekanslarını göster
